0d060a5b85367bbe7821dda8e8583fb9ecb01c9c
Rutwik Gulhane <Rutwik.Gulhane@BRILLIO.COM>
2022-05-06 09:25:18 -0700

    vaccination app

---
+++ src/Components/Home/home.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return(
+
+        <div>
+
+            <br /><br />
+
+            <h1 className="text-center">VACCINATION APPLICATION</h1>
+     
+
+        </div>
+
+    )
+
+}
+
+export default Home;
